f4bfc46aa0dc97a3fba07deb0e96add43eef1443
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2023-11-23 23:24:06 -0500

    ADD title to RightDOM.js

--- src/Modules/DOM/RightDOM.js
+++ src/Modules/DOM/RightDOM.js
@@ -1,8 +1,22 @@
 import TodoCard from './TodoCard.js';
 
 function rightDOM(todosAlone, projects) {
-
+    let container = document.createElement('div');
+    let title = document.createElement('div');
     let element = document.createElement('div');
+
+    title.innerHTML = todosAlone.title;
+
+    title.style = `
+        text-align: center;
+        margin: 0;
+        padding: 10px 0;
+        background-color: lightblue;
+        font-size: 2rem;
+        weight: bold;
+        letter-spacing: 32px;
+        text-transform: uppercase
+    `
 
     element.style = `
     height: 100%;
@@ -15,7 +29,9 @@ function rightDOM(todosAlone, projects) {
         element.appendChild(TodoCard(todo));
     });
 
-    return element;
+    container.appendChild(title);
+    container.appendChild(element);
+    return container;
 }
 
 export default rightDOM;
